1778c88fdc72a07ac55653eafde4564b247a7a98
pcb <pcb@unit.no>
2020-08-24 10:34:14 +0200

    testing reading secrets from jenkins

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,13 +3,18 @@ pipeline {
     tools {
         nodejs "nodejs10110"
     }
-
+    environment {
+        CURATOR_TOKEN    = credentials('dlr_test_curator_token')
+        RAIRAIRAI    = "RAIRAIRAI"
+    }
     stages {
         stage('Install') {
             steps {
                 sh "node -v"
                 sh "npm -v"
                 sh 'npm install'
+                echo "Credentials : ${env.RAIRAIRAI}"
+                echo "Credentials : ${env.CURATOR_TOKEN}"
             }
         }
         stage('Run tests') {
