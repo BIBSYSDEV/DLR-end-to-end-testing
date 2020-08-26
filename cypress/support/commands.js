3eb4e374874ccc4516d5c740ccf631bb5d1a419c
pcb <pcb@unit.no>
2020-08-26 12:15:40 +0200

    removed test-message to screen

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,5 @@
 import "cypress-localstorage-commands";
 
 Cypress.Commands.add('login_curator', () => {
-
-    throw new Error('TOKEN! ' + Cypress.env('CURATOR_TOKEN'));
-
     cy.visit(`/loginRedirect?token=${Cypress.env('CURATOR_TOKEN')}`);
 });
