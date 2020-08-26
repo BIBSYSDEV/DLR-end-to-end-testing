99e2dc51198f53dbd0054a5417d9b4692e772839
pcb <pcb@unit.no>
2020-08-26 12:24:36 +0200

    testing jenkins again

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,7 +1,7 @@
 import "cypress-localstorage-commands";
 
 Cypress.Commands.add('login_curator', () => {
-    const link = `/loginRedirect?token=${Cypress.env('CURATOR_TOKEN')}`
+    const link = `/loginRedirect?token=${Cypress.env('CYPRESS_CURATOR_TOKEN')}`
     cy.visit(link);
     throw new Error(link);
 });
